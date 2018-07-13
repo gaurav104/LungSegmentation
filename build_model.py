3bd6e821494e75191b917b8d5bdbafc5f6241cd3
Gaurav Patel <gouravpatel8@gmail.com>
2018-07-13 20:50:46 +0530

    build model compiled

--- build_model.py
+++ build_model.py
@@ -134,3 +134,40 @@ class SegNet(nn.Module):
         x11d = self.conv11d(x12d)
 
         return x11d
+
+
+import torch
+import torch.nn as nn
+import torch.nn.functional as F
+
+from unet_parts import *
+
+
+class UNet(nn.Module):
+    def __init__(self, n_channels, n_classes):
+        super(UNet, self).__init__()
+        self.inc = inconv(n_channels, 64)
+        self.down1 = down(64, 128)
+        self.down2 = down(128, 256)
+        self.down3 = down(256, 512)
+        self.down4 = down(512, 512)
+        self.up1 = up(1024, 256)
+        self.up2 = up(512, 128)
+        self.up3 = up(256, 64)
+        self.up4 = up(128, 64)
+        self.outc = outconv(64, n_classes)
+        self.Dropout = nn.Dropout(0.5)
+
+    def forward(self, x):
+        x1 = self.inc(x)
+        x2 = self.down1(x1)
+        x3 = self.down2(x2)
+        x3 = self.Dropout(x3)
+        x4 = self.down3(x3)
+        x5 = self.down4(x4)
+        x = self.up1(x5, x4)
+        x = self.up2(x, x3)
+        x = self.up3(x, x2)
+        x = self.up4(x, x1)
+        x = self.outc(x)
+        return x
